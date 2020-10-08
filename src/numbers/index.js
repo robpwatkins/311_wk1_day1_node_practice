566827c4d4980965193ff52e6b6b68cf1081c22c
Rob W <robpwatkins@gmail.com>
2020-10-08 13:17:14 -0500

    reverting to orig

--- src/numbers/index.js
+++ src/numbers/index.js
@@ -1,24 +1,16 @@
 const isEven = (num) => {
-  if (num % 2 === 0) {
-    return true;
-  }
+  // write code for numbers.isEven
 
 }
 
 const sum = (arr) => {
-  return arr.reduce((a, b) => a + b);
+  // write code for numbers.sum
+
 }
 
 const comboSum = (arr, sum) => {
-  for (let i = 0; i < arr.length; i++) {
-    for (let n = 0; n < arr.length; n++) {
-      if (arr[i] + arr[n] === sum) {
-        return true;
-      } else {
-        return false;
-      }
-    }
-  }
+  // write code for numbers.comboSum
+
 }
 
 module.exports = {
